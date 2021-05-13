36cce06eb39d3390ad765d65a32d2191b8db8908
fenglixiong <fenglixiong@flashhold.com>
2021-05-13 17:15:12 +0800

    gataway

--- netty-chat-gateway/src/main/java/com/flx/netty/chat/gateway/filter/UrlFilter.java
+++ netty-chat-gateway/src/main/java/com/flx/netty/chat/gateway/filter/UrlFilter.java
@@ -5,7 +5,6 @@ import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.server.RequestPath;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
