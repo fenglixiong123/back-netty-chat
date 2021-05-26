9f4b2d2580ad7428116c8a8cdd1d2b06ec6ea065
fenglixiong <fenglixiong@flashhold.com>
2021-05-26 17:10:54 +0800

    整理admin架构

--- netty-chat-admin/src/main/java/com/flx/netty/chat/admin/common/security/handler/LoginSuccessfulHandler.java
+++ netty-chat-admin/src/main/java/com/flx/netty/chat/admin/common/security/handler/LoginSuccessfulHandler.java
@@ -23,7 +23,7 @@ public class LoginSuccessfulHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         log.info("=======>Login successful !");
-        ResultResponse.printSuccess(response,"恭喜，登录成功！",authentication);
+        ResultResponse.printSuccess(response,"恭喜，登录成功！",authentication.getPrincipal());
     }
 
 }
